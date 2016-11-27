dc7305eeade850a2292913925a129c385c854d01
Mohammed Chisti <93mchistI@gmail.com>
2016-11-26 21:38:52 -0500

    Completed

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,51 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+
+  name = []
+  data.each{|key, value|
+    value.each{|key, value|
+      value.each{|key, value|
+        name.push(key)
+      }
+    }
+  }
+
+  name.uniq!
+
+  name.each{|name|
+    new_data[name] = {}
+    data.each{|key, value|
+      new_data[name][key] = []
+    }
+  }
+
+  data.each{|key, value|
+    if key == :color
+      value.each{|key, value|
+        new_data.each{|k, v|
+          if value.include?(k)
+            new_data[k][:color].push(key.to_s)
+          end
+        }
+      }
+    elsif key == :gender
+      value.each{|key, value|
+        new_data.each{|k, v|
+          if value.include?(k)
+            new_data[k][:gender].push(key.to_s)
+          end
+        }
+      }
+    elsif key == :lives
+      value.each{|key, value|
+        new_data.each{|k, v|
+          if value.include?(k)
+            new_data[k][:lives].push(key.to_s)
+          end
+        }
+      }
+    end
+  }
+
+  new_data
 end
